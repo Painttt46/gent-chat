d234de921d5806a7ec1026da3444a591acaa078b
Painttt46 <vpaint345@gmail.com>
2025-10-03 11:33:43 +0700

    SDCx
    SDA

--- services/gemini.js
+++ services/gemini.js
@@ -1,13 +1,13 @@
 // services/gemini.service.js
 
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import { calendarFunction, createEventFunction, findAvailableTimeFunction, systemInstruction } from '../config/gent_config.js';
+import { calendarFunction, createEventFunction, systemInstruction } from '../config/gent_config.js';
 
 export async function getGeminiResponse(apiKey, modelName, history) {
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({
         model: modelName,
-        tools: [{ functionDeclarations: [calendarFunction, createEventFunction, findAvailableTimeFunction] }],
+        tools: [{ functionDeclarations: [calendarFunction, createEventFunction] }],
         systemInstruction: systemInstruction
     });
 
